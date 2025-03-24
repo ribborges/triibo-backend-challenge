32e743e89ad0f7fd8258098965f0e4486a2c57c5
Richard Borges <ribborges@outlook.com>
2025-03-24 18:14:46 -0300

    feat: set up Express server with CORS and cookie parsing

--- src/server.js
+++ src/server.js
@@ -1,1 +1,21 @@
-console.log('Hello, world!');
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+import { port } from '#root/config/env.js';
+
+// Create and setup express app
+const app = express();
+
+app.use(cors({
+    credentials: true,
+    methods: ['GET, POST, PUT, DELETE']
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
