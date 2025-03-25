b0edd821f32822c633b1d31d33f104f38ceb0493
Richard Borges <ribborges@outlook.com>
2025-03-25 19:42:31 -0300

    fix: update import statement for omdbApiKey to include file extension

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { omdbApiKey } from '#root/config/env';
+import { omdbApiKey } from '#root/config/env.js';
 
 const api = axios.create({
     baseURL: 'http://www.omdbapi.com',
