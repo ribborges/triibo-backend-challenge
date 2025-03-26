92bb6692feafe8d4425bb7cd55c415ac2c338c6e
Richard Borges <ribborges@outlook.com>
2025-03-25 21:55:27 -0300

    feat: implement authentication helper functions for password hashing and token generation

---
+++ src/helpers/auth.js
@@ -0,0 +1,18 @@
+import crypto from 'crypto';
+import jwt from 'jsonwebtoken';
+
+import { secret } from '#root/config/env.js';
+
+const random = () => crypto.randomBytes(128).toString('base64');
+
+const hashPassword = (password, salt) => {
+    return crypto.createHmac('sha256', [salt, password].join('/')).update(secret).digest('hex');
+}
+
+const genToken = (id) => {
+    return jwt.sign({ id }, secret, {
+        expiresIn: "30d",
+    });
+};
+
+export { random, hashPassword, genToken };
