ee1dee82b423a145863735f1abc163621881821e
Richard Borges <ribborges@outlook.com>
2025-03-25 21:54:56 -0300

    feat: add secret to environment configuration exports

--- src/config/env.js
+++ src/config/env.js
@@ -4,5 +4,6 @@ dotenv.config();
 
 const port = process.env.PORT || 8000;
 const omdbApiKey = process.env.OMDB_API_KEY;
+const secret = process.env.SECRET;
 
-export { port, omdbApiKey };
+export { port, omdbApiKey, secret };
